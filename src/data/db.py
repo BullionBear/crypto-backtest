2ad140f1b09c7df24ab1ee0cd62837dd2044abb0
yite <coastq22889@icloud.com>
2024-03-03 10:43:19 +0800

    singleton pattern

--- src/data/db.py
+++ src/data/db.py
@@ -1,11 +1,19 @@
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 
-mongo_url = 'mongodb://bullionbear:Sunshine4Jellybean@ec2-57-181-58-168.ap-northeast-1.compute.amazonaws.com:27017'
+class MongoSingleton:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(MongoSingleton, cls).__new__(cls)
+            mongo_url = 'mongodb://bullionbear:Sunshine4Jellybean@ec2-57-181-58-168.ap-northeast-1.compute.amazonaws.com:27017'
+            cls._instance.client = MongoClient(mongo_url)
+        return cls._instance.client
 
 
-def get_client():  # Singleton
-    return MongoClient(mongo_url)
+def get_client():
+    return MongoSingleton()
 
 
 def test_mongo_url(url):
